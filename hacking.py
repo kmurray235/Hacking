cbf90a83dc8c629647dc732b2277e94ab5261f9b
kmurray235 <53528984+kmurray235@users.noreply.github.com>
2024-10-31 15:51:37 -0400

    editing the spaces in hacking.py

--- hacking.py
+++ hacking.py
@@ -1,22 +1,15 @@
 import time
-
 import random
-
 
 print("🔍 Initiating mainframe hack...")
 
-
 for i in range(1, 101, random.randint(5,30)):
-
      time.sleep(0.2)
      print(f"Hacking {i}% complete...")
-
 
 time.sleep(1)
 print("💻 Accessing classified files...")
 time.sleep(1)
-
-
 
 secrets = ["🍕 Pineapple does belong on pizza.",
 
@@ -27,7 +20,6 @@ secrets = ["🍕 Pineapple does belong on pizza.",
            "🤖 Robots secretly want to be friends.",
 
            "🚀 Aliens think Earth is a tourist trap."]
-
 
 time.sleep(1)
 print("🔓 Files accessed! Secret revealed:")
