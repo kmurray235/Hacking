bd481b7748e8031048fec1725dad9f415861fc75
kmurray235 <53528984+kmurray235@users.noreply.github.com>
2024-10-31 15:32:01 -0400

    created hacking.py

---
+++ hacking.py
@@ -0,0 +1,41 @@
+import time
+
+import random
+
+
+print("🔍 Initiating mainframe hack...")
+
+
+for i in range(1, 101, random.randint(5,30)):
+
+ time.sleep(0.2)
+
+print(f"Hacking {i}% complete...")
+
+
+time.sleep(1)
+
+print("💻 Accessing classified files...")
+
+time.sleep(1)
+
+
+
+secrets = ["🍕 Pineapple does belong on pizza.",
+
+       "🕶️ Spies don't actually wear sunglasses indoors.",
+
+       "📀 Blockbuster is planning a comeback.",
+
+       "🤖 Robots secretly want to be friends.",
+
+       "🚀 Aliens think Earth is a tourist trap."]
+
+
+time.sleep(1)
+
+print("🔓 Files accessed! Secret revealed:")
+
+print(random.choice(secrets))
+
+
